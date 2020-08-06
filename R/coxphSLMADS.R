5b387df984d546c994c18212c5b057d6d67b14e7
neelsoumya <neel.soumya@gmail.com>
2020-08-06 12:44:58 +0100

    changing documentation coxph

--- R/coxphSLMADS.R
+++ R/coxphSLMADS.R
@@ -50,8 +50,12 @@ coxphSLMADS<-function(search.filter=NULL, dataName=NULL)
       
       # formula2use <- formula
       # mod.glm.ds <- stats::glm(formula2use, family=family, x=TRUE, control=stats::glm.control(maxit=1), contrasts=NULL, data=dataTable)
-      cxph_serverside <- survival::coxph(formula = survival::Surv(time = survtime, event = cens) ~  female,
+      #cxph_serverside <- survival::coxph(formula = survival::Surv(time = survtime, event = cens) ~  female,
+      #                                   data = dataTable)
+      cxph_serverside <- survival::coxph(formula = survival::Surv(time = SURVTIME, event = EVENT) ~  age,
                                          data = dataTable)
+      
+      
       
       cat('\n Hello World from server-side function coxphSLMADS() in dsBase \n')
       temp_str <- 'Hello World from server-side dsBase::coxphSLMADS()'
