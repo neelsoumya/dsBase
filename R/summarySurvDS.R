dde08aa9a17a09db638d9e0820d6181612df448f
Soumya Banerjee <neel.soumya@gmail.com>
2020-09-07 13:03:36 +0100

    coxph wip

---
+++ R/summarySurvDS.R
@@ -0,0 +1,72 @@
+#' 
+#' @title Returns summary of survival object.
+#' @description returns a summary of the survival Surv() object from the server side environment.
+#' @details Serverside aggregate function {coxphSLMADS} called by clientside function.
+#' {ds.summary}.
+#' returns a summary of the survival Surv() object.
+#' This request is not disclosive.
+#' For further details see help for {ds.summary} function.
+#' @param formula either NULL or a character string (potentially including '*'
+#' wildcards) specifying a formula.
+#' @param dataName character string of name of data frame
+#' @return a summary of the Cox proportional hazards from the server side environment from the server side environment.
+#' @author Soumya Banerjee (2020).
+#' @export
+coxphSLMADS<-function(formula=NULL, dataName=NULL)
+{
+      
+      errorMessage <- "No errors"
+      
+      #########################################################################
+      # DataSHIELD MODULE: CAPTURE THE nfilter SETTINGS                       #
+      thr <- listDisclosureSettingsDS()                                       #
+      #nfilter.tab<-as.numeric(thr$nfilter.tab)                               #
+      #nfilter.glm<-as.numeric(thr$nfilter.glm)                               #
+      #nfilter.subset<-as.numeric(thr$nfilter.subset)                         #
+      nfilter.string <- as.numeric(thr$nfilter.string)                        #
+      nfilter.tab    <- as.numeric(thr$nfilter.tab)                           #
+      nfilter.glm    <- as.numeric(thr$nfilter.glm)                           #
+      #nfilter.stringShort<-as.numeric(thr$nfilter.stringShort)               #
+      #nfilter.kNN<-as.numeric(thr$nfilter.kNN)                               #
+      #datashield.privacyLevel<-as.numeric(thr$datashield.privacyLevel)       #
+      #########################################################################
+      
+      # get the value of the 'data' and 'weights' parameters provided as character on the client side
+      if(is.null(dataName)){
+         dataTable <- NULL 
+      }else{
+         dataTable <- eval(parse(text=dataName), envir = parent.frame())
+      }
+      
+      
+      
+      
+      
+      cxph_serverside <- survival::coxph(formula = formula,
+                                         data = dataTable)
+      
+      ###########################
+      # disclosure checks
+      ###########################
+      # check if model oversaturated
+      num_parameters  <- length(cxph_serverside$coefficients)
+      num_data_points <- cxph_serverside$n
+      
+      if(num_parameters > (nfilter.glm * num_data_points) )
+      {
+            #glm.saturation.invalid<-1
+            #errorMessage.gos<-paste0("ERROR: Model is oversaturated (too many model parameters relative to sample size)",
+            #                 "leading to a possible risk of disclosure - please simplify model. With ",
+            #                 num.p," parameters and nfilter.glm = ",round(nfilter.glm,4)," you need ",
+            #                 round((num.p/nfilter.glm),0)," observations")
+            return("ERROR: Model is oversaturated (too many model parameters relative to sample size)")
+      }
+      
+      # cat('\n Hello World from server-side function coxphSLMADS() in dsBase \n')
+      # temp_str <- 'Hello World from server-side dsBase::coxphSLMADS()'
+      # outlist <- paste0(search.filter, temp_str)
+      # return(outlist)
+      return(summary(cxph_serverside))
+}
+#AGGREGATE FUNCTION
+# coxphSLMADS
