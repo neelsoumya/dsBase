e126f32895e7d4ee156a9ca17c7342ee66826d1a
Soumya Banerjee <neel.soumya@gmail.com>
2020-10-27 09:06:23 +0000

    Update plotsurvfitDS.R

--- R/plotsurvfitDS.R
+++ R/plotsurvfitDS.R
@@ -71,118 +71,15 @@ plotsurvfitDS<-function(formula = NULL,
          stop("The formula must be set for use in survival::coxph()", call.=FALSE)
       } 	
 	
-      ####################################################################	
-      # Logic for parsing formula: since this need to be passed
-      ####################################################################	
-      # Put pipes back into formula
-      #formula = as.formula(paste(formula,collapse="|"))
-      formula <- Reduce(paste, deparse(formula))
-      formula <- gsub("sssss", "survival::Surv(", formula, fixed = TRUE)
-      formula <- gsub("lll", "=", formula, fixed = TRUE)
-      formula <- gsub("xxx", "|", formula, fixed = TRUE)
-      formula <- gsub("yyy", "(", formula, fixed = TRUE)
-      formula <- gsub("zzz", ")", formula, fixed = TRUE)
-      formula <- gsub("ppp", "/", formula, fixed = TRUE)
-      formula <- gsub("qqq", ":", formula, fixed = TRUE)
-      formula <- gsub("rrr", ",", formula, fixed = TRUE)
-
-      # convert back to formula
-      formula <- stats::as.formula(formula)
-      
-      ########################################
-      # reconstruct control object
-      ########################################
-      if (is.null(control))
-      {
-            # if the value is null, then substitute default values which is 
-            #   survival::coxph.control()
-            control <- survival::coxph.control()
-      }
-      else
-      {
-            # reconstruct after passing this through parser
-            ####################################################################	
-            # Logic for parsing formula: since this need to be passed
-            ####################################################################	
-           
-            # Put pipes back into formula
-            #formula = as.formula(paste(formula,collapse="|"))
-	    
-            control <- Reduce(paste, deparse(control))
-	    # remove the extra ~ bbbb passed here
-	    #	this ~ bbbb needs to be passed because
-	    #   everything needs to be passed as formula
-	    #	and an expression of the form a ~ b is required	  
-	    control <- gsub("~ bbbb", "", control, fixed = TRUE)
-	    control <- gsub("~", "", control, fixed = TRUE)
-	    control <- gsub("bbbb", "", control, fixed = TRUE)     
-            control <- gsub("aaaaa", "survival::coxph.control(", control, fixed =  TRUE)
-   	    control <- gsub("xxx", "|", control, fixed = TRUE)
-   	    control <- gsub("yyy", "(", control, fixed = TRUE)
-   	    control <- gsub("zzz", ")", control, fixed = TRUE)
-	    control <- gsub("ppp", "/", control, fixed = TRUE)
-	    control <- gsub("qqq", ":", control, fixed = TRUE)
-	    control <- gsub("rrr", ",", control, fixed = TRUE)
-	    #control <- gsub("", " ",    control, fixed = TRUE)
-	    control <- gsub("lll", "=", control, fixed = TRUE)
-            
-            # use eval to construct an object of type survival::coxph.control()
-            # control <- eval(parse(text=control), envir = parent.frame())
-        
-      }  
-  	
-      ########################################
-      # construct call to survival::coxph()
-      ########################################
-      # if init is NULL, then do not call coxph with init parameter
-      if (!is.null(init))
-      {
-              cxph_serverside <- survival::coxph(formula = formula,
-                                                 data = dataTable,
-                                                 weights = weights,
-                                                 init = init,
-                                                 ties = ties,
-                                                 singular.ok = singular.ok,
-                                                 model = model,
-                                                 x = x,
-                                                 y = y#,
-                                                 #control = eval(parse(text=as.character(control)))
-                                                )
-      }
-      else
-      {
-              cxph_serverside <- survival::coxph(formula = formula,
-                                                 data = dataTable,
-                                                 weights = weights,
-                                                 ties = ties,
-                                                 singular.ok = singular.ok,
-                                                 model = model,
-                                                 x = x,
-                                                 y = y#,
-                                                 #control = eval(parse(text=as.character(control)))
-                                                 )
-      }
       
       ###########################
       # disclosure checks
       ###########################
       # check if model oversaturated
-      num_parameters  <- length(cxph_serverside$coefficients)
-      num_data_points <- cxph_serverside$n
-      
-      # if number of parameters greater than 0.2 * number of data points, then error
-      if(num_parameters > (nfilter.glm * num_data_points) )
-      {
-            # glm.saturation.invalid<-1
-            # errorMessage.gos<-paste0("ERROR: Model is oversaturated (too many model parameters relative to sample size)",
-            #                 "leading to a possible risk of disclosure - please simplify model. With ",
-            #                 num.p," parameters and nfilter.glm = ",round(nfilter.glm,4)," you need ",
-            #                 round((num.p/nfilter.glm),0)," observations")
-            return("ERROR: Model is oversaturated (too many model parameters relative to sample size)")
-      }
+      survfit_model_variable = eval(parse(text=formula), envir = parent.frame())
       
       
-      return(summary(cxph_serverside))
+      return(summary(survfit_model_variable))
 }
 #AGGREGATE FUNCTION
 # plotsurvfitDS
